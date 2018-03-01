f9b15ddeadd9b3a74782cdaa828ff46ac5bf55b7
Lillianaloha <32822385+Lillianaloha@users.noreply.github.com>
2018-03-01 12:27:22 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,9 +15,7 @@ import sys
 import os.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir, 'code')))
 
-import mysql.connector
-
-
+# import mysql.connector
 
 import main
 import unittest
