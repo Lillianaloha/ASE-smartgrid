fdeffc45bbc1d790879915a7f22758e7e3c23c2c
AndrewQuijano <andrewquijano92@gmail.com>
2018-04-21 12:12:31 -0400

    Generator second ticker works, unable to figure out why server isn't getting data now though...

--- generate/generate.c
+++ generate/generate.c
@@ -14,17 +14,22 @@
 # define Vin 110
 # define SQRT2 1.14142
 # define SQRT3 1.73205
+# define ON 1
+# define OFF 0
 # define THREADS 16
 
+// Power Generator
+static int omega = 1;
+static int phi = 60;
+
 // Lily...Connect to this Port
-unsigned short serverPort = 8001;
+unsigned short serverPort = 8000;
 
 //Current Epoch
 time_t epoch;
 
 static pthread_t thread_pool[THREADS];
-char ipAddress [15] = "127.0.0.1";
-static int flag = 1;
+static int status = ON;
 struct data * d;
 
 Sigfunc * signal_intr(int signo, Sigfunc *func)
@@ -66,7 +71,7 @@ Sigfunc * signal(int signo, Sigfunc *func)
 
 static void breakLoop(int signo)
 {
-    flag = 0;
+    status = OFF;
 }
 
 void * generateData()
@@ -74,15 +79,33 @@ void * generateData()
     // For every second...
     // Generate new values...
 
+    int t = 0;
+    time_t current;
+    time_t previous;
     printf("Data Generator started...\n");
-    int t = 1;
-    int w = 1;
-    int phi = 60;
 
-    while (flag)
+    time(&current);
+    current -= epoch;
+
+    while (status)
     {
-        // pthread_mutex_lock(&(d -> mutex));
-        pthread_rwlock_wrlock(&d -> rwlock);
+	// http://www.ece.k-state.edu//~starret/581/3phase.html
+	// https://www.electronics-tutorials.ws/accircuits/reactive-power.html
+	previous = current;
+	// Update Current...
+	time(&current);
+    	current -= epoch;
+
+	//If a second hasn't elapsed wait a bit more...
+	if(current == previous)
+	{
+            continue;		
+	}
+	printf("Data being generated\n");
+	//Update Time
+	++t;
+	
+	pthread_rwlock_wrlock(&d -> rwlock);
         // va = \sqrt(2) V_in cos(wt + phi)
         // vb = \sqrt(2) V_in cos(wt + phi - 120)
         // vc = \sqrt(2) V_in cos(wt + phi + 120)
@@ -91,11 +114,10 @@ void * generateData()
         // Voltage is default 110, +/- 5%    
         // 104.5 - 114.5 Volts
         
-        d -> Va = (int)(SQRT2 * (double) Vin * cos((double) ( w * t + phi)));
-        d -> Vb = (int)(SQRT2 * (double) Vin * cos((double) ( w * t + phi - 120)));
-        d -> Vc = (int)(SQRT2 * (double) Vin * cos((double) ( w * t + phi + 120)));
+        d -> Va = (int)(SQRT2 * (double) Vin * cos((double) ( omega * t + phi)));
+        d -> Vb = (int)(SQRT2 * (double) Vin * cos((double) ( omega * t + phi - 120)));
+        d -> Vc = (int)(SQRT2 * (double) Vin * cos((double) ( omega * t + phi + 120)));
 
-  
         // Current 0 - 50 A
         d -> Ia = rand() % 50;
         d -> Ib = rand() % 50;
@@ -109,7 +131,7 @@ void * generateData()
         d -> PhaseC_Power = d -> Vc * d -> Ic;
             
 
-        //Reactive Power: Irms * Vrms * 
+        //Reactive Power: Irms * Vrms
         d -> ReactivePower += 1;
         d -> PhaseA_ReactivePower += 1;
         d -> PhaseB_ReactivePower += 1;
@@ -117,7 +139,6 @@ void * generateData()
         d -> Consumed_Power += 1;
         d -> Sold_Power += 1;
 
-        //pthread_mutex_unlock(&(d -> mutex));
         pthread_rwlock_unlock(&d -> rwlock);
     }
     printf("Generator shutting down\n");
@@ -133,16 +154,17 @@ void * run(void * connection)
     //This socket connects generator and master computer...
     int clntSock = (int) temp;
     */
-    int Va, Vb, Vc;
-    int Ia, Ib, Ic;
-    int Total_Power, Total_FundamentalPower;
-    int PhaseA_Power, PhaseB_Power, PhaseC_Power;
-    int ReactivePower;
-    int PhaseA_ReactivePower, PhaseB_ReactivePower, PhaseC_ReactivePower;
-    int Consumed_Power, Sold_Power;
 
+    // Get time to read
+    // Get sampling rate: 1 reading per second, 2 second, etc.
+    char time_input [4];
+    char sampling_input[4];
+    char * temp;        //Shift to remove padding on 0s
     char printData [255] = "";
-    int ctr = 0;
+
+    time_t current;
+    time_t thread_epoch;
+    time_t previous;
 
     int servSock = (long) connection;
 
@@ -158,26 +180,57 @@ void * run(void * connection)
 
     // Get time to read
     // Get sampling rate: 1 reading per second, 2 second, etc.
-    int time = 0;       //(in seconds)
-    int sampling = 0;   //(in seconds)
+    unsigned int time_out = 0;           //(in seconds)
+    unsigned int sampling = 0;           //(in seconds)
     
 //-------------------Get Time------------------------
     printf("Waiting for reading time\n");
-    if(read(clntSock, &time, sizeof(int)) < 0)
+    if(read(clntSock, time_input, 4) < 0)
     {
-        printf("Error at reading time\n");
+        printf("Error at reading time.\n");
 	pthread_exit(NULL);
     }
-    printf("Thread received time rate: %d\n", time);
-
+    printf("Thread received time rate: %s\n", time_input);
+    
+    //Remove leading 0s...
+    temp = time_input;
+    for(int i = 0; i < 4; i++)
+    {
+        if(time_input[i] == '0')
+        {
+            temp++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("value of time_input is: %s\n", temp);
+    time_out = atoi(temp);
 //----------------Get Sampling Rate-----------------
     printf("Waiting for reading sampling rate\n");
-    if(read(clntSock, &sampling, sizeof(int)) < 0)
+    if(read(clntSock, sampling_input, 4) < 0)
     {
-        printf("Error at reading sampling rate\n");
+        printf("Error at reading sampling rate.\n");
 	pthread_exit(NULL);
     }
-    printf("Thread received sampling rate: %d\n", sampling);
+    printf("Thread received sampling rate: %s\n", sampling_input);
+    
+    //Remove leading 0s...
+    temp = sampling_input;
+    for(int i = 0; i < 4; i++)
+    {
+        if(sampling_input[i] == '0')
+        {
+            temp++;
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("value of time_input is: %s\n", temp);
+    sampling = atoi(temp);
 
 //---------------Get Extra Data for direct outward communication----------------
 /*
@@ -212,44 +265,60 @@ void * run(void * connection)
     }
     remoteSock = createClientSocket(remote_ip, remote_portNum);      
 */   
+ 
+    // Start While
+    time(&current);
+    time(&thread_epoch);
+    current-= thread_epoch;
 
     while(true)  
-    {  
-	//pthread_mutex_lock(&(d -> mutex));
-    	pthread_rwlock_rdlock(&d -> rwlock);
-        Va = d -> Va;
-    	Vb = d -> Vb;
-    	Vc = d -> Vc;
-    	Ia = d -> Ia;
-    	Ib = d -> Ib;
-    	Ic = d -> Ic;
-    	Total_Power = d -> Total_Power;
-    	Total_FundamentalPower = d -> Total_FundamentalPower;
-    	PhaseA_Power = d -> PhaseA_Power; 
-    	PhaseB_Power = d -> PhaseB_Power;
-    	PhaseC_Power = d -> PhaseC_Power;
-    	ReactivePower = d -> ReactivePower;
-    	PhaseA_ReactivePower = d -> PhaseA_ReactivePower;
-    	PhaseB_ReactivePower = d -> PhaseB_ReactivePower;
-    	PhaseC_ReactivePower = d -> PhaseC_ReactivePower;
-    	Consumed_Power = d -> Consumed_Power;
-    	Sold_Power = d -> Sold_Power;
-    	//pthread_mutex_unlock(&(d -> mutex));
-        pthread_rwlock_unlock(&d -> rwlock);
-
-    	sprintf(printData, "{%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d}",
-    	        Va, Vb, Vc, Ia, Ib, Ic, Total_Power, Total_FundamentalPower, PhaseA_Power,
-    	        PhaseB_Power, PhaseC_Power, ReactivePower, PhaseA_ReactivePower, PhaseB_ReactivePower, 
-    	        PhaseC_ReactivePower, Consumed_Power, Sold_Power);
-    	fprintf(stdout, "%s\n", printData);
-    	Send(clntSock, printData);
-        if(ctr == time)
-        {
-            break;
+    {
+	// Prev is holding last iteration of current
+	previous = current;
+	
+	// Update and check...
+	time(&current);
+        current -= thread_epoch;
+	
+	// A second did not pass...
+	if(current == previous)
+	{
+            continue;
+	}
+        //printf("Current is: %ld and Previous is: %ld\n", current, previous);
+	//printf("Start to read, time out is: %d and sampling is: %d\n", time_out, sampling);
+        
+   	// Sample data now!
+	if(current % sampling == 0)
+	{
+            printf("Begin Reading data\n");
+            //pthread_rwlock_rdlock(&d -> rwlock);
+            sprintf(printData, "{%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d,%d}",
+    	        d->Va, d->Vb, d->Vc, d->Ia, d->Ib, d->Ic, d->Total_Power, d->Total_FundamentalPower, 
+		d->PhaseA_Power, d->PhaseB_Power, d->PhaseC_Power, d->ReactivePower, 
+		d->PhaseA_ReactivePower, d->PhaseB_ReactivePower, d->PhaseC_ReactivePower, 
+		d->Consumed_Power, d->Sold_Power);
+            //pthread_rwlock_unlock(&d -> rwlock);
+            
+            fprintf(stdout, "%s\n", printData);
+            
+            /*
+            if(Send(clntSock, printData) < 0)
+            {
+                printf("Error sending!");
+            }
+            */
+            //send(clntSock, printData, sizeof(printData), 0);
         }
-        ++ctr;   
+
+	// Time is up!
+	if(current >= time_out)
+        {
+            printf("Time is up!\n");
+            break;
+	}
     }
-    printf("Listening Thread complete!");
+    printf("Listening Thread complete!\n");
     return NULL;
 }
 
@@ -266,6 +335,30 @@ int main(int argc, char **argv)
 
     }
 
+    // Command Line Arguments...
+    if (argc == 1)
+    {
+        // Need arguments?
+    }
+    // Pass in Server Port
+    else if (argc == 2)
+    {
+        serverPort = atoi(argv[1]);
+    }
+    // Pass in Server Port, omega (frequency)
+    else if (argc == 3)
+    {
+        serverPort = atoi(argv[1]);
+        omega = atoi(argv[2]);
+    }
+    // Pass in Server Port, omega (frequency), phi
+    else
+    {
+        serverPort = atoi(argv[1]);
+        omega = atoi(argv[2]);
+        phi = atoi(argv[3]);
+    }
+
     srand(time(NULL));
 
     //Listen for incoming requests
@@ -280,7 +373,9 @@ int main(int argc, char **argv)
     }
     data_init(d);
 
-    printf("Smart Meter Program Initialized!\n");
+    printf("Smart Meter Program Initialized! Clock initialized!\n");
+    time(&epoch);
+
     pthread_t generatorThread;
     pthread_create(&generatorThread, NULL, &generateData, NULL);
    
@@ -309,6 +404,10 @@ int main(int argc, char **argv)
     }
     // for (;;)
      */    
+
+
+    //Wait...CTRL+C
+    while(status);
     
     //Clean up Program...
     pthread_join(generatorThread, NULL);
