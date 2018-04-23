6f5996a7f58a4cbea33293339d29bc0d2156fb2f
Lillianaloha <32822385+Lillianaloha@users.noreply.github.com>
2018-04-23 16:28:36 -0400

    increase port number automatically

--- front-end/zqq.php
+++ front-end/zqq.php
@@ -8,12 +8,19 @@
 //echo $time_rate;
 //echo $sampling_rate;
     
-    $a = "10";
-$c = intval($a);
-if($c == 10){echo $c;}
+//    $a = "10";
+//$c = intval($a);
+//if($c == 10){echo $c;}
+//
 
+$myfile = fopen("portnumber.txt", "r");
+$txt = fread($myfile,filesize("portnumber.txt"));
+$port = intval($txt);
+echo $port;
+$content = str_replace($txt,strval($port+1),$txt);
+$myfile2 = fopen("portnumber.txt", "w");
+fwrite($myfile2,$content);
 
-    
     
     
     
